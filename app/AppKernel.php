fc6c16bd639574f91c8c9e54265e99560e629b09
patrickderoubaix <cendres@gmail.com>
2013-01-26 13:15:59 +0100

    ajout bundle et début configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Itkg\Bundle\PhpRedmonBundle\ItkgPhpRedmonBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Itkg/Bundle/PhpRedmonBundle/ItkgPhpRedmonBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Itkg\Bundle\PhpRedmonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItkgPhpRedmonBundle extends Bundle
+{
+}
